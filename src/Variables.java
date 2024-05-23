1e777de4147126a92a5d6823f52c7c279113ef2d
Christopher C Fleming <63308959+ChristopherCFleming@users.noreply.github.com>
2024-05-22 20:33:36 -0400

    Complete exercise on variable reassigning

--- src/Variables.java
+++ src/Variables.java
@@ -1,11 +1,24 @@
 public class Variables {                     // 1. A class
 
     public static void main(String[] args) { // 2. The main method
-          int linesOfCode;
-          linesOfCode = 227;
-          int dogCount = 94;
-          int catCount = 112;
-          int petCount = dogCount + catCount;
-          System.out.println(petCount);
+//          int linesOfCode;
+//          linesOfCode = 227;
+//          int dogCount = 94;
+//          int catCount = 112;
+//          int petCount = dogCount + catCount;
+//          System.out.println(petCount);
+//          int tacos = 12;
+//          tacos = 29;
+//          System.out.println(tacos);
+//          tacos = 7;
+//          System.out.println(tacos);
+          int tacos = 17;
+          int hotdogs = 23;
+          int total = tacos + hotdogs;
+
+          System.out.println(total);
+          System.out.println(tacos);
+
+
     }
 }
