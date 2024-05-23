c54e5d85155c7ca55c2b0bf2feb3dae51565d8e0
Christopher C Fleming <63308959+ChristopherCFleming@users.noreply.github.com>
2024-05-23 01:21:47 -0400

    Finish variables and types sandbox

---
+++ src/Strings.java
@@ -0,0 +1,40 @@
+public class Strings {
+
+    public static void main(String[] args) {
+//        System.out.println("hello strings");
+//        String message = "baked apples";
+//        System.out.println("hello strings");
+//        String message;
+//        message = "My dog has fleas\nand \"charm.\"";
+//        System.out.println(message);
+//        String gluedMessage = "school buses are kind of " + "orange";
+//        System.out.println(gluedMessage);
+//        String redAndBlue = "red, blue, ";
+//        String primaryColors = redAndBlue + "yellow";
+//        System.out.println(primaryColors);
+//        int dozen = 12;
+//        boolean likesWalks = true;
+//        double n = -9.75;
+//
+//        String message = dozen + " is my favorite number.";
+//        System.out.println(message);
+//        message = "Do you feel like a walk? " + likesWalks;
+//        System.out.println(message);
+//        String message = "baked apples";
+//        int characterCount = message.length();
+//        System.out.println(characterCount);
+//        char fifthChar = message.charAt(4);
+//        System.out.println(fifthChar);
+//        String fruit = message.substring(6);
+//        fruit = message.substring(5, 5);
+//        System.out.println(fruit);
+//        String message = "baked apples";
+//        String aBecomesI = message.replace('a', 'i');
+//        System.out.println(aBecomesI);
+//        int three = 3;
+//        int four = 3;
+//        System.out.println(three == four);
+
+
+    }
+}
