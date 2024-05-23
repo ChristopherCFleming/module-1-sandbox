3ea96885bb7d21ef309f4d2a89573fae7a8fd167
Christopher C Fleming <63308959+ChristopherCFleming@users.noreply.github.com>
2024-05-22 20:22:41 -0400

    Complete int variable practice

--- src/Variables.java
+++ src/Variables.java
@@ -1,6 +1,11 @@
 public class Variables {                     // 1. A class
 
     public static void main(String[] args) { // 2. The main method
-          // 3. Our code
+          int linesOfCode;
+          linesOfCode = 227;
+          int dogCount = 94;
+          int catCount = 112;
+          int petCount = dogCount + catCount;
+          System.out.println(petCount);
     }
 }
