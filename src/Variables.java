624f6b9b24dc6e1000952799226061e182d4fe19
Christopher C Fleming <63308959+ChristopherCFleming@users.noreply.github.com>
2024-05-22 21:56:23 -0400

    Finish Variables module in Java

--- src/Variables.java
+++ src/Variables.java
@@ -12,12 +12,34 @@ public class Variables {                     // 1. A class
 //          System.out.println(tacos);
 //          tacos = 7;
 //          System.out.println(tacos);
-          int tacos = 17;
-          int hotdogs = 23;
-          int total = tacos + hotdogs;
+//          int tacos = 17;
+//          int hotdogs = 23;
+//          int total = tacos + hotdogs;
+//
+//          System.out.println(total);
+//          System.out.println(tacos);
+//          byte b = 56;
+//          short s = 1024;
+//          int i = 314;
+//          long lng = -256;
+//          float f = 3.14f;
+//          double d = 3.14;
+//          System.out.println(f);
+//            double value = 9.22 - 1.1;
+//            double d = 3.32;
+//            System.out.println(value);
+//            System.out.println(d);
+//            double result = value - d;
+//            System.out.println(result);
+//        int product = 20 * 4;
+//        int i = 3;
+//        int result = product * i;
+//        System.out.println(result);
+//        long value = 100L / 2L;
+//        value = 100L / 3L;
+//        System.out.println(value);
 
-          System.out.println(total);
-          System.out.println(tacos);
+
 
 
     }
